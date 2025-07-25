b03e69a80bf7987663b63802ceb3d69bb24db7fa
Neil <neil.gudwala@gmail.com>
2025-07-25 17:17:45 +0530

    initialised vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
     <Analytics /> 
+    <SpeedInsights/>
   </BrowserRouter>
 );
